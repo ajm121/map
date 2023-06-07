b37588ff1a66e67da8728c50bbb5a2e98ea788ca
ajm121 <127277722+ajm121@users.noreply.github.com>
2023-06-07 14:01:37 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 app.get("/", (req, res) => {
-  const appey = process.env.APP_KEY; // 환경변수에서 APP_KEY를 가져옴
+  const appkey = process.env.APP_KEY; // 환경변수에서 APP_KEY를 가져옴
   const html = `
 <!DOCTYPE html>
 <html>
